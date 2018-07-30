779371c2b8b3e4366b58c48139e27ef745f2485c
likun <1617455243@qq.com>
2018-07-30 22:48:38 +0800

    '排行页面'

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -3,6 +3,9 @@
   <div class="song-list">
     <ul>
       <li @click="selectItem(song, index)" v-for="(song,index) in songs" class="item">
+        <div class="rank" v-show="rank">
+          <span :class="getRankCls(index)">{{getRankText(index)}}</span>
+        </div>
         <div class="content">
           <h2 class="name">{{song.name}}</h2>
           <p class="desc">{{getDesc(song)}}</p>
@@ -18,6 +21,10 @@ export default {
     songs: {
       type: Array,
       default: []
+    },
+    rank: {
+      type: Boolean,
+      default: false
     }
   },
   methods: {
@@ -26,6 +33,17 @@ export default {
     },
     selectItem(item, index) {
       this.$emit('select', item, index)
+    },
+    getRankCls (index) {
+      if (index <= 2) {
+        return `icon icon${index}`
+      }
+      return 'text'
+    },
+    getRankText (index) {
+      if (index > 2) {
+        return index + 1
+      }
     }
   }
 }
